77f7d9580d1c77d9cfbee35a76736f12ea27c3b0
HKrobinvai <hkrobin49@gmail.com>
2026-05-31 21:45:11 +0600

    add the new express command

--- Exppress--lerarnign/src/server.ts
+++ Exppress--lerarnign/src/server.ts
@@ -1,10 +1,14 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app : Application = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req : Request, res : Response) => {
+  res.status(200).json({
+    "message" : "Express Server",
+    "author" : "next level"
+  })
 })
+app.post()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
