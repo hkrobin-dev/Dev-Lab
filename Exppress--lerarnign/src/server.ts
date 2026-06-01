3f8ad9546491724222035531ab73624ca7300053
HKrobinvai <hkrobin49@gmail.com>
2026-06-01 12:50:22 +0600

    adding the neon pool pass

--- Exppress--lerarnign/src/server.ts
+++ Exppress--lerarnign/src/server.ts
@@ -1,6 +1,16 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
 const app : Application = express()
 const port = 5000
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended : true}));
+
+const pool = new Pool({
+  connectionString : "postgresql://neondb_owner:npg_H86eovmUIlzP@ep-hidden-cloud-apk6oo7r-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
 
 app.get('/', (req : Request, res : Response) => {
   res.status(200).json({
@@ -8,7 +18,16 @@ app.get('/', (req : Request, res : Response) => {
     "author" : "next level"
   })
 })
-app.post()
+app.post("/", async(req : Request , res : Response)=>{
+  // console.log(req.body);
+  const {name,email,password,} = req.body;
+  res.status(201).json({
+    message : "Created",
+    data : {
+      name,email
+    }, 
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
