ec0cf25e330e2efcb0e0f03509675cf9c0ffe556
HKrobinvai <hkrobin49@gmail.com>
2026-06-11 20:35:40 +0600

    add the jwt token access in get method

--- Exppress--lerarnign/src/app.ts
+++ Exppress--lerarnign/src/app.ts
@@ -4,20 +4,14 @@ import { userRoute } from "./modules/user/user.rotue";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import fs from "fs"
+import logger from "./middleware/logger";
 const app: Application = express()
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log('Method - URL - Time:',req.method, req.url, Date.now());
-  const log = `\nMethod -> ${req.method} Time -> ${Date.now()}  URL -> ${req.url}\n`
-  fs.appendFile('logger.txt', log,(err)=>{
-    console.log(err)
-  })
-  next();  
-});
+app.use(logger);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
---
+++ Exppress--lerarnign/src/middleware/auth.ts
@@ -0,0 +1,19 @@
+import type { NextFunction, Request, Response } from "express"
+
+const auth = ()=>{
+    return async  (req :Request,res : Response,next : NextFunction)=>{
+    // console.log(req.headers);
+    const token = req.headers.authorization;
+
+    if(!token){
+        res.status(401).json({
+        success: false,
+        message: "Unauthorized   User",
+      
+    });
+    }
+    next()
+}
+}
+
+export default auth
