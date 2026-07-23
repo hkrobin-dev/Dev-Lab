829ce7ac33bcfb1dfaa100aecd575af3024e41e5
HKrobinvai <hkrobin49@gmail.com>
2026-07-23 11:34:22 +0600

    over the main setup of project press prisma

---
+++ project-prisma-press-backend/src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app.js";
+import config from "./config/index.js";
+import { prisma } from "./lib/prisma.js";
+
+const PORT = config.PORT;
+async function main() {
+    try{
+        await prisma.$connect();
+        console.log(`Connected to the database successfully.`);
+       app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }catch (error) {
+        console.error("Error starting server:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+main()
